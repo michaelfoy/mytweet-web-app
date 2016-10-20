cb20b4bc2805670f735714e44ff0c3b83e2e9913
michaelfoy <mikkfoy@gmail.com>
2016-10-20 19:55:26 +0100

    myTweet feed added

--- app/controllers/mytweet.js
+++ app/controllers/mytweet.js
@@ -3,8 +3,24 @@ const Tweet = require('../models/tweet');
 const User = require('../models/user');
 
 exports.home = {
-  handler: (request, reply) => {
-    reply.view('home', { title: 'MyTweet' });
+  handler: function (request, reply) {
+    Tweet.find({}).populate('tweeter').then(tweets => {
+      tweets.sort(function (a, b) {
+        if (a.date > b.date) {
+          return -1;
+        }
+        if (a.date < b.date) {
+          return 1;
+        }
+        return 0;
+      });
+      reply.view('home', {
+        title: 'MyTweet',
+        tweets: tweets,
+      });
+    }).catch(err => {
+      reply.redirect('/');
+    });
   },
 };
 
@@ -15,7 +31,7 @@ exports.newTweet = {
       let data = request.payload;
       const tweet = new Tweet(data);
       tweet.tweeter = user._id;
-      tweet.date = 'datePlaceholder'
+      tweet.date = getDate();
       return tweet.save();
     }).then(newTweet => {
       reply.redirect('/home');
@@ -24,3 +40,21 @@ exports.newTweet = {
     });
   },
 };
+
+function getDate() {
+  let date = new Date()
+  let dd = date.getDay() + 16;
+  let mm = date.getMonth() + 1;
+  let hh = date.getHours();
+  let min = date.getMinutes();
+  const yy = date.getFullYear();
+
+  const nums = [mm, dd, hh, min]
+  for ( let i = 0; i <  nums.length; i++) {
+    if (nums[i] < 10) {
+      nums[i] = '0' + nums[i]
+    }
+  }
+  const dateStr = nums[2] + ':' + nums[3] + ' - ' + nums[1] + '/' + nums[0] + '/' + yy;
+  return dateStr;
+}
